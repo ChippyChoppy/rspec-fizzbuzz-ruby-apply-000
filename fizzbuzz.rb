8848e6efc6778b9f7aa253bbcb7d5abf64cfd032
Rebecca Robbins <rebeccamargaret13@gmail.com>
2020-09-08 22:36:26 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,14 +2,14 @@
 def fizzbuzz(int)
 2.6.1 :012?>   if int % 3 ==0
 2.6.1 :013?>     puts "Fizz"
-2.6.1 :014?>     end
-2.6.1 :015?>   if int % 5 == 0
-2.6.1 :016?>     puts "Buzz"
-2.6.1 :017?>     end
-2.6.1 :018?>   if int % 3 == 0 && int % 5 == 0
-2.6.1 :019?>     puts "Fizzbuzz"
-2.6.1 :020?>     end
-2.6.1 :021?>   if int % 3 != 0 && int % 5 != 0
-2.6.1 :022?>     puts "nil"
-2.6.1 :023?>     end
-2.6.1 :024?>   end
+   end
+  if int % 5 == 0
+    puts "Buzz"
+    end
+ if int % 3 == 0 && int % 5 == 0
+    puts "Fizzbuzz"
+    end
+  if int % 3 != 0 && int % 5 != 0
+    puts "nil"
+   end
+end
