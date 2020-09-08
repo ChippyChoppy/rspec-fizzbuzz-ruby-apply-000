f18a7789ec84db06aceaa35ce8809c1726ad8758
Rebecca Robbins <rebeccamargaret13@gmail.com>
2020-09-08 22:35:56 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
-def fizzbuzz
-  
-end
+def fizzbuzz(int)
+2.6.1 :012?>   if int % 3 ==0
+2.6.1 :013?>     puts "Fizz"
+2.6.1 :014?>     end
+2.6.1 :015?>   if int % 5 == 0
+2.6.1 :016?>     puts "Buzz"
+2.6.1 :017?>     end
+2.6.1 :018?>   if int % 3 == 0 && int % 5 == 0
+2.6.1 :019?>     puts "Fizzbuzz"
+2.6.1 :020?>     end
+2.6.1 :021?>   if int % 3 != 0 && int % 5 != 0
+2.6.1 :022?>     puts "nil"
+2.6.1 :023?>     end
+2.6.1 :024?>   end
