63fa7919245d83ad4f9b9f410a5079e698158499
Rebecca Robbins <rebeccamargaret13@gmail.com>
2020-09-08 22:41:58 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,13 +2,15 @@
 def fizzbuzz(int)
   if int % 3 == int % 5 == 0
     return "Fizzbuzz"
+  end
   
    elsif int % 3 ==0
      return "Fizz"
    end
-  if int % 5 == 0
+  elsif int % 5 == 0
     return "Buzz"
     end
- 
-    end
+  else
+    NIL
+    
 end
