dd461be5cafd5d55364c3fc1ffb152fc03818b97
Rebecca Robbins <rebeccamargaret13@gmail.com>
2020-09-08 22:41:35 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 def fizzbuzz(int)
+  if int % 3 == int % 5 == 0
+    return "Fizzbuzz"
+  
    if int % 3 ==0
      return "Fizz"
    end
   if int % 5 == 0
     return "Buzz"
     end
- if int % 3 == int % 5 == 0
-    return "Fizzbuzz"
+ 
     end
 end
